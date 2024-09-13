ef1f95b4ae5ec6001eff46e4fbdf8c3fbf76a3f1
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2024-09-14 00:12:07 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let bracketMap = new Map(bracketsConfig);
+
+  for(let item of str){
+      if(bracketMap.has(item)) {
+          stack.push(item);
+      } else  {
+          if(stack.length > 0) {
+              let result = stack.pop();
+              if(item !== bracketMap.get(result)){    
+                  return false;
+              } 
+          } else if (stack.length === 0) {
+              return false;
+          }
+      } 
+  }
+
+  if(stack.length === 0) {
+      return true;
+  } else {
+      return false;
+  };
 }
